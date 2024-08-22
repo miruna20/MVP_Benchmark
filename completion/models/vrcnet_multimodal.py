BUGFIX: computation of emd for arch and body, ADDED: additional results evaluation

completion/models/vrcnet_multimodal.py
@@ -485,8 +485,6 @@ class Model(nn.Module):
 
     def forward(self, x_pcd, x_labelmap=None, gt=None, prefix="train", mean_feature=None, alpha=None):
 
-        # TODO for exp2 find out where we do the concatenation before passing to ReNET
-
         if prefix == "train":
             y = gather_points(gt.transpose(1, 2).contiguous(), furthest_point_sample(gt, self.num_points))
 
@@ -725,10 +723,12 @@ class Model(nn.Module):
             cds_p_arch = []
             cds_t_arch = []
             f1s_arch = []
+            emds_arch = []
 
             cds_p_body = []
             cds_t_body = []
             f1s_body = []
+            emds_body = []
 
             # fine_cpu.shape[0] --> number of shapes in the current batch
             nr_shapes = fine_cpu.shape[0]
@@ -793,6 +793,13 @@ class Model(nn.Module):
                 cds_t_body.append(cd_t_body)
                 f1s_body.append(f1_body)
 
+                if self.eval_emd:
+                    emd_arch = calc_emd(completion_arch, gt_arch, eps=0.004, iterations=3000)
+                    emds_arch.append(emd_arch)
+                    emd_body = calc_emd(completion_body,gt_body, eps=0.004, iterations=3000)
+                    emds_body.append(emd_body)
+
+
             cd_p_arch = torch.stack(cds_p_arch)
             cd_p_arch = torch.reshape(cd_p_arch, (fine_cpu.shape[0],))
             cd_t_arch = torch.stack(cds_t_arch)
@@ -808,13 +815,21 @@ class Model(nn.Module):
             f1_body = torch.reshape(f1_body, (fine_cpu.shape[0],))
 
             if self.eval_emd:
-                emd = calc_emd(fine, gt, eps=0.004, iterations=3000)
-                emd_arch = calc_emd(fine, gt, eps=0.004, iterations=3000)
-                emd_body = calc_emd(fine, gt, eps=0.004, iterations=3000)
+                emd_arch = torch.stack(emds_arch)
+                emd_arch = torch.reshape(emd_arch, (fine_cpu.shape[0],))
+
+                emd_body = torch.stack(emds_body)
+                emd_body = torch.reshape(emd_body, (fine_cpu.shape[0],))
             else:
-                emd = 0
                 emd_arch = 0
                 emd_body = 0
+
+
+
+            if self.eval_emd:
+                emd = calc_emd(fine, gt, eps=0.004, iterations=3000)
+            else:
+                emd = 0
 
             # compute the metrics for the whole vertebral shape
             cd_p, cd_t, f1 = calc_cd(fine, gt, calc_f1=True)

completion/test.py
@@ -6,7 +6,9 @@ import argparse
 import numpy as np
 import h5py
 import subprocess
-
+import torch
+import matplotlib.pyplot as plt
+import seaborn as sns
 import munch
 import yaml
 from train_utils import *
@@ -20,6 +22,44 @@ warnings.filterwarnings("ignore")
 device = 'cuda'
 device_ids = [0]
 
+def plot_category_boxplots(category_metrics_results, metrics, cat_name, log_dir):
+    """
+    Plot boxplots for each category and save them to the specified directory.
+
+    Args:
+    - category_metrics_results (dict): Dictionary with category-wise metric results.
+    - metrics (list): List of metric names.
+    - cat_name (list): List of category names.
+    - log_dir (str): Directory to save the plots.
+    """
+    for metric in metrics:
+        plt.figure(figsize=(12, 8))
+
+        # Prepare data for boxplot
+        data = [category_metrics_results[cat][metric] if 'f1' in metric else [arr *10000 for arr in category_metrics_results[cat][metric]]for cat in cat_name]
+        labels = [f' L{i + 1}' for i in range(len(data))]
+        labels.append('All Categories')
+
+        combined_data = np.concatenate(data)
+        data.append(combined_data)
+
+        # Check if data is empty
+        if any(len(d) == 0 for d in data):
+            logging.warning(f"No data for metric {metric}. Skipping boxplot.")
+            continue
+
+        # Create a boxplot
+        plt.boxplot(data, labels = labels)
+        plt.xticks(ticks=np.arange(1, len(data) + 1), labels=labels)
+        plt.xlabel('Category')
+        plt.ylabel(metric)
+        plt.title(f'Boxplot of {metric} for each category')
+
+        # Save the plot
+        plt.tight_layout()
+        plt.savefig(os.path.join(log_dir, f'{metric}_boxplot.png'))
+        plt.close()
+
 def test():
     logging.info(str(args))
 
@@ -31,7 +71,7 @@ def test():
                                     val_path=args.path_to_val_dataset,
                                     test_path=args.path_to_test_dataset,
                                     apply_trafo=args.apply_trafo,
-                                    sigma = args.sigma,
+                                    sigma=args.sigma,
                                     Xray_labelmap=use_labelmaps,
                                     prefix=prefix,
                                     num_partial_scans_per_mesh=args.num_partial_scans_per_mesh)
@@ -40,11 +80,10 @@ def test():
     dataset_length = len(dataset_test)
     logging.info('Length of test dataset:%d', len(dataset_test))
 
-    # load model
+    # Load model
     model_module = importlib.import_module('.%s' % args.model_name, 'models')
 
     modelPath = args.load_model
-
 
     net = torch.nn.DataParallel(model_module.Model(args), device_ids=device_ids)
     net.to(device)
@@ -52,26 +91,26 @@ def test():
     logging.info("%s's previous weights loaded." % args.model_name)
     net.eval()
 
-    # metrics we would like to compute
-    if(args.eval_emd):
-        metrics = ['cd_p', 'cd_p_arch', 'cd_p_body', 'cd_t', 'cd_t_arch', 'cd_t_body', 'emd', 'emd_arch','emd_body', 'f1', 'f1_arch','f1_body']
+    # Metrics we would like to compute
+    if args.eval_emd:
+        metrics = ['cd_p', 'cd_p_arch', 'cd_p_body', 'cd_t', 'cd_t_arch', 'cd_t_body', 'emd', 'emd_arch', 'emd_body', 'f1', 'f1_arch', 'f1_body']
     else:
         metrics = ['cd_p', 'cd_p_arch', 'cd_p_body', 'cd_t', 'cd_t_arch', 'cd_t_body', 'f1', 'f1_arch', 'f1_body']
 
-    # dictionary with all of the metrics
+    # Dictionary with all of the metrics
     test_loss_meters = {m: AverageValueMeter() for m in metrics}
 
-    # number of samples per class
+    # Number of samples per class
     number_samples_per_class = dataset_test.number_per_classes
     num_partial_scans_per_category = dataset_test.num_partial_scans_per_mesh
 
-    # number of categories
+    # Number of categories
     num_categories = len(number_samples_per_class)
 
-    # metrics for each category present (in our case 5 lumbar levels x 4 metrics we would like to compute)
+    # Metrics for each category present
     test_loss_cat = torch.zeros([num_categories, len(metrics)], dtype=torch.float32).cuda()
 
-    # number of samples from each category
+    # Number of samples from each category
     cat_num = torch.ones([num_categories, 1], dtype=torch.float32).cuda()
 
     cat_name = ['L1', 'L2', 'L3', 'L4', 'L5']
@@ -83,28 +122,23 @@ def test():
 
     with torch.no_grad():
         results_list = []
-        emd = []
-        emd_arch = []
-        emd_body = []
-        cd_p = []
-        cd_p_arch = []
-        cd_p_body = []  # Add list for vertebral body CD_p
-        cd_t = []
-        cd_t_arch = []
-        cd_t_body = []  # Add list for vertebral body CD_t
-        f1 = []
-        f1_arch = []
-        f1_body = []  # Add list for vertebral body F1
-        gts_aligned = []
-        inputs_aligned = []
+
+        # Added dictionary to collect all metric results
+        all_metrics_results = {m: [] for m in metrics}
+
+        # Added dictionary to collect category-wise results
+        category_metrics_results = {cat: {m: [] for m in metrics} for cat in cat_name}
+
+        labels_seen = []
         for i, data in enumerate(dataloader_test):
+
 
             label, partial_pcd, labelmap, gt = data
 
             partial_pcd = partial_pcd.float().to(device)
             partial_pcd = partial_pcd.transpose(2, 1).contiguous()
 
-            if (use_labelmaps):
+            if use_labelmaps:
                 labelmap = labelmap.float().to(device)
                 labelmap = labelmap.transpose(2, 1).contiguous()
 
@@ -112,32 +146,19 @@ def test():
 
             result_dict = net(partial_pcd, labelmap, gt, prefix=prefix)
 
-            # Update average metrics
+            # Update average metrics and collect results
             for k, v in test_loss_meters.items():
                 v.update(result_dict[k].mean().item())
+                all_metrics_results[k].append(result_dict[k].cpu().numpy())
 
-            # Sum up results for each metric
+            # Sum up results for each metric and collect category-wise results
             for j, l in enumerate(label):
                 for ind, m in enumerate(metrics):
                     test_loss_cat[int(l), ind] += result_dict[m][int(j)]
+                    category_metrics_results[cat_name[int(l)]][m].append(result_dict[m][int(j)].cpu().numpy())
 
             # Append shape completion results
             results_list.append(result_dict['result'].cpu().numpy())
-            if args.eval_emd:
-                emd.append(result_dict['emd'].cpu().numpy())
-                emd_arch.append(result_dict['emd_arch'].cpu().numpy())
-                emd_body.append(result_dict['emd_body'].cpu().numpy())  # Append vertebral body EMD
-            cd_p.append(result_dict['cd_p'].cpu().numpy())
-            cd_p_arch.append(result_dict['cd_p_arch'].cpu().numpy())
-            cd_p_body.append(result_dict['cd_p_body'].cpu().numpy())  # Append vertebral body CD_p
-            cd_t.append(result_dict['cd_t'].cpu().numpy())
-            cd_t_arch.append(result_dict['cd_t_arch'].cpu().numpy())
-            cd_t_body.append(result_dict['cd_t_body'].cpu().numpy())  # Append vertebral body CD_t
-            f1.append(result_dict['f1'].cpu().numpy())
-            f1_arch.append(result_dict['f1_arch'].cpu().numpy())
-            f1_body.append(result_dict['f1_body'].cpu().numpy())  # Append vertebral body F1
-            gts_aligned.append(result_dict['gt'].cpu().numpy())
-            inputs_aligned.append(result_dict['inputs'].cpu().numpy())
 
             if i % args.step_interval_to_print == 0:
                 logging.info('test [%d/%d]' % (i, dataset_length / args.batch_size))
@@ -153,63 +174,43 @@ def test():
             }
             for ind, m in enumerate(metrics):
                 scale_factor = 1 if m in ['f1', 'f1_arch', 'f1_body'] else 10000
-                category_log += ' %s: %f' % (m, test_loss_cat[i, ind] / cat_num[i] * scale_factor)
-                value = test_loss_cat[i, ind] / cat_num[i] * scale_factor
-                row_data[m] = value.cpu().numpy()[0]
+                mean_value = test_loss_cat[i, ind] / cat_num[i] * scale_factor
+                std_dev = np.std(category_metrics_results[cat_name[i]][m]) * scale_factor
+                median_value = np.median(category_metrics_results[cat_name[i]][m]) * scale_factor
+                mean_std_median_str = f"{mean_value.item():.4f} ± {std_dev:.4f} (Median: {median_value:.4f})"
+                category_log += ' %s: %s' % (m, mean_std_median_str)
+                row_data[m] = mean_std_median_str
 
             table_data.append(row_data)
 
         logging.info(category_log)
-        # Log the text data
         wandb.log({"Per category results": category_log})
 
         logging.info('Overview results:')
         overview_log = ''
         overview_results = []
+
         for metric, meter in test_loss_meters.items():
             scale_factor = 1 if metric in ['f1', 'f1_arch', 'f1_body'] else 10000
-            overview_log += '%s: %f ' % (metric, meter.avg * scale_factor)
-            overview_results.append((metric,meter.avg * scale_factor))
+            mean_value = meter.avg * scale_factor
+            metric_values = np.concatenate(all_metrics_results[metric], axis=0) * scale_factor
+            std_dev = np.std(metric_values)
+            median_value = np.median(metric_values)
+            mean_std_median_str = f"{mean_value:.4f} ± {std_dev:.4f} (Median: {median_value:.4f})"
+            overview_log += '%s: %s ' % (metric, mean_std_median_str)
+            overview_results.append((metric, mean_std_median_str))
 
         logging.info(overview_log)
-        wandb.log({"Overview Results": category_log})
+        wandb.log({"Overview Results": overview_log})
 
         # Concatenate all results
         all_results = np.concatenate(results_list, axis=0)
-        if args.eval_emd:
-            all_emd = np.concatenate(emd, axis=0)
-            all_emd_arch = np.concatenate(emd_arch, axis=0)
-            all_emd_body = np.concatenate(emd_body, axis=0)  # Concatenate vertebral body EMD
-        all_cd_p = np.concatenate(cd_p, axis=0)
-        all_cd_p_arch = np.concatenate(cd_p_arch, axis=0)
-        all_cd_p_body = np.concatenate(cd_p_body, axis=0)  # Concatenate vertebral body CD_p
-        all_cd_t = np.concatenate(cd_t, axis=0)
-        all_cd_t_arch = np.concatenate(cd_t_arch, axis=0)
-        all_cd_t_body = np.concatenate(cd_t_body, axis=0)  # Concatenate vertebral body CD_t
-        all_f1 = np.concatenate(f1, axis=0)
-        all_f1_arch = np.concatenate(f1_arch, axis=0)
-        all_f1_body = np.concatenate(f1_body, axis=0)  # Concatenate vertebral body F1
-        all_gt = np.concatenate(gts_aligned, axis=0)
-        all_inputs = np.concatenate(inputs_aligned, axis=0)
 
         # Write results to HDF5 file
-        with h5py.File(log_dir + '/results.h5', 'w') as f:
+        with h5py.File(os.path.join(log_dir, 'results.h5'), 'w') as f:
             f.create_dataset('results', data=all_results)
-            if args.eval_emd:
-                f.create_dataset('emd', data=all_emd)
-                f.create_dataset('emd_arch', data=all_emd_arch)
-                f.create_dataset('emd_body', data=all_emd_body)  # Write vertebral body EMD
-            f.create_dataset('cd_p', data=all_cd_p)
-            f.create_dataset('cd_p_arch', data=all_cd_p_arch)
-            f.create_dataset('cd_p_body', data=all_cd_p_body)  # Write vertebral body CD_p
-            f.create_dataset('cd_t', data=all_cd_t)
-            f.create_dataset('cd_t_arch', data=all_cd_t_arch)
-            f.create_dataset('cd_t_body', data=all_cd_t_body)  # Write vertebral body CD_t
-            f.create_dataset('f1', data=all_f1)
-            f.create_dataset('f1_arch', data=all_f1_arch)
-            f.create_dataset('f1_body', data=all_f1_body)  # Write vertebral body F1
-            f.create_dataset('gt', data=all_gt)
-            # f.create_dataset('input_aligned', data=all_inputs)  # Uncomment if needed
+            for metric in metrics:
+                f.create_dataset(metric, data=np.concatenate(all_metrics_results[metric], axis=0))
 
         # Create submission zip file
         cur_dir = os.getcwd()
@@ -218,6 +219,8 @@ def test():
         _, _ = process.communicate()
         print("Submission file has been saved to %s/submission.zip" % (log_dir))
 
+    # Plot and save the boxplots
+    plot_category_boxplots(category_metrics_results, metrics, cat_name, log_dir)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Test config file')
@@ -229,7 +232,7 @@ if __name__ == "__main__":
     args = munch.munchify(yaml.safe_load(open(config_path)))
 
     wandb.login(key="845cb3b94791a8d541b28fd3a9b2887374fe8b2c")
-    run = wandb.init(project="Multimodal Shape Completion",tags=['inference'])
+    run = wandb.init(project="Multimodal Shape Completion", tags=['inference'])
     wandb.config.update(args)
 
     if not args.load_model:
@@ -237,16 +240,16 @@ if __name__ == "__main__":
 
     output_directory = "results"
     timestr = time.strftime("%Y%m%d-%H%M%S")
-    log_dir = os.path.join(output_directory,args.name_folder_to_save_results)
+    log_dir = os.path.join(output_directory, args.name_folder_to_save_results)
 
-    # in case on the log_dir does not exist
+    # In case the log_dir does not exist
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
     logging.basicConfig(level=logging.INFO, handlers=[logging.FileHandler(os.path.join(log_dir, 'test.log')),
                                                       logging.StreamHandler(sys.stdout)])
 
     logging.info(f"Project URL: {run.get_project_url()}")
-    logging.info(f"Run URL: { run.get_url()}")
+    logging.info(f"Run URL: {run.get_url()}")
     logging.info(f"Local directory: {run.dir}")
 
     test()